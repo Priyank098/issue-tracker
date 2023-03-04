39a51345046cf18e9a66d60e6b8801c410013cfe
Priyank098 <Priyank.a@antino.io>
2023-03-04 12:37:32 +0530

    bug fix

--- src/models/user.js
+++ src/models/user.js
@@ -45,7 +45,6 @@ userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
 
-    delete userObject.token
     delete userObject.password
 
     return userObject
